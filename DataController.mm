82acb48fdcfad78bc761b331ef5917113f68e620
fG <reverser@put.as>
2012-10-13 22:55:17 +0100

    Fix the crash with fat binaries that include armv7s architecture. Problem is that NXGetArchInfoFromCpuType() doesn't know this arch.

--- DataController.mm
+++ DataController.mm
@@ -984,9 +984,8 @@ NSString * const MVStatusTaskTerminated           = @"MVStatusTaskTerminated";
   [node.userInfo setObject:layout forKey:MVLayoutUserInfoKey];
   
   [layouts addObject:layout];
-  
   for (uint32_t nimg = 0; nimg < fat_header->nfat_arch; ++nimg)
-  {      
+  {
     // need to make copy for byte swapping
     struct fat_arch fat_arch;
     [fileData getBytes:&fat_arch range:NSMakeRange(sizeof(struct fat_header) + nimg * sizeof(struct fat_arch), sizeof(struct fat_arch))];
@@ -1002,7 +1001,9 @@ NSString * const MVStatusTaskTerminated           = @"MVStatusTaskTerminated";
       // need to make copy for byte swapping
       struct mach_header mach_header;
       [fileData getBytes:&mach_header range:NSMakeRange(fat_arch.offset, sizeof(struct mach_header))];
-      
+      // XXX: workaround because NXGetArchInfoFromCpuType() doesn't know CPU_SUBTYPE_ARM_V7S and returns
+      //      invalid data leading to a crash on byteorder access
+      if (fat_arch.cpusubtype == CPU_SUBTYPE_ARM_V7S) fat_arch.cpusubtype = CPU_SUBTYPE_ARM_V7K;
       enum NXByteOrder byteorder = NXGetArchInfoFromCpuType(fat_arch.cputype,fat_arch.cpusubtype)->byteorder;
       if (byteorder == NX_BigEndian)
       {
