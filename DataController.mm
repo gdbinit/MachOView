62f8cea4e1b3a4a7a06bc9023fcd4f0bcd802c12
Cédric Luthi <cedric.luthi@gmail.com>
2014-01-06 15:41:31 +0100

    Workaround `NXGetArchInfoFromCpuType` not knowing about arm64

--- DataController.mm
+++ DataController.mm
@@ -1052,9 +1052,10 @@ NSString * const MVStatusTaskTerminated           = @"MVStatusTaskTerminated";
       // need to make copy for byte swapping
       struct mach_header mach_header;
       [fileData getBytes:&mach_header range:NSMakeRange(fat_arch.offset, sizeof(struct mach_header))];
-      // XXX: workaround because NXGetArchInfoFromCpuType() doesn't know CPU_SUBTYPE_ARM_V7S and returns
-      //      invalid data leading to a crash on byteorder access
+      // XXX: workaround because NXGetArchInfoFromCpuType() doesn't know neither CPU_SUBTYPE_ARM_V7S nor
+      //      CPU_TYPE_ARM | CPU_ARCH_ABI64 and returns invalid data leading to a crash on byteorder access
       if (fat_arch.cpusubtype == CPU_SUBTYPE_ARM_V7S) fat_arch.cpusubtype = CPU_SUBTYPE_ARM_V7K;
+      if (fat_arch.cputype == (CPU_TYPE_ARM | CPU_ARCH_ABI64)) fat_arch.cputype = CPU_TYPE_ARM;
       enum NXByteOrder byteorder = NXGetArchInfoFromCpuType(fat_arch.cputype,fat_arch.cpusubtype)->byteorder;
       if (byteorder == NX_BigEndian)
       {
