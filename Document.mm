899a2f01f043f8ba59f0527f662b0eb39635d568
Keith Smiley <keithbsmiley@gmail.com>
2021-08-17 22:35:52 -0700

    Update for Xcode 12.5
    Small changes I had to make to build successfully with Xcode 12.5

--- Document.mm
+++ Document.mm
@@ -450,25 +450,27 @@ enum ViewType
 {
   if ([notification object] == dataController)
   {
-    NSString * threadState = [[notification userInfo] objectForKey:MVStatusUserInfoKey];
-    if ([threadState isEqualToString:MVStatusTaskStarted] == YES)
-    {
-      if (OSAtomicIncrement32(&threadCount) == 1)
+    dispatch_async(dispatch_get_main_queue(), ^{
+      NSString * threadState = [[notification userInfo] objectForKey:MVStatusUserInfoKey];
+      if ([threadState isEqualToString:MVStatusTaskStarted] == YES)
       {
-        [progressIndicator setUsesThreadedAnimation:YES];
-        [progressIndicator startAnimation:nil];
-        [stopButton setHidden:NO];
+        if (OSAtomicIncrement32(&threadCount) == 1)
+        {
+          [progressIndicator setUsesThreadedAnimation:YES];
+          [progressIndicator startAnimation:nil];
+          [stopButton setHidden:NO];
+        }
       }
-    }
-    else if ([threadState isEqualToString:MVStatusTaskTerminated] == YES)
-    {
-      if (OSAtomicDecrement32(&threadCount) == 0)
+      else if ([threadState isEqualToString:MVStatusTaskTerminated] == YES)
       {
-        [progressIndicator stopAnimation:nil]; 
-        [statusText setStringValue:@""];
-        [stopButton setHidden:YES];
+        if (OSAtomicDecrement32(&threadCount) == 0)
+        {
+          [progressIndicator stopAnimation:nil];
+          [statusText setStringValue:@""];
+          [stopButton setHidden:YES];
+        }
       }
-    }
+    });
   }
 }
 
