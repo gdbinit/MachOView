eb2e3aa84ec8125815486910893c3d650bc6a3f0
dalonng <dalonng@users.noreply.github.com>
2023-07-03 00:54:00 +0800

    add MVLayout

--- Packages/MachOView/Sources/MachOView/MVLayout.swift
+++ Packages/MachOView/Sources/MachOView/MVLayout.swift
@@ -7,7 +7,44 @@
 
 import Foundation
 
+public class MVDataController {}
+public class MVArchiver {}
+
 public final class MVLayout: NSObject {
   weak var rootNode: MVNode?
-  weak var dataController: 
+  weak var dataController: MVDataController?
+  let imageOffset: UInt64
+  let imageSize: UInt64
+  let backgroundThread: Thread
+  let archiver: MVArchiver
+  
+  init(rootNode: MVNode? = nil,
+       dataController: MVDataController? = nil,
+       imageOffset: UInt64,
+       imageSize: UInt64,
+       backgroundThread: Thread,
+       archiver: MVArchiver) {
+    self.rootNode = rootNode
+    self.dataController = dataController
+    self.imageOffset = imageOffset
+    self.imageSize = imageSize
+    self.backgroundThread = backgroundThread
+    self.archiver = archiver
+  }
 }
+
+
+//- (void const *)        imageAt:(uint64_t)location NS_RETURNS_INNER_POINTER;
+//- (void)                printException:(NSException *)exception caption:(NSString *)caption;
+//- (BOOL)                is64bit;
+//- (void)                doMainTasks;
+//- (void)                doBackgroundTasks;
+//- (NSString *)          convertToRVA: (NSString *)offsetStr;
+//- (MVNode *)            findNodeByUserInfo:(NSDictionary *)userInfo;
+//
+//- (MVNode *)            createDataNode:(MVNode *)parent
+//                               caption:(NSString *)caption
+//                              location:(uint64_t)location
+//                                length:(uint64_t)length;
+//
+//@end
